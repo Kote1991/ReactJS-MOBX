00586255ac0aabe85b78d2d4752ace10b633831e
Kotresh\Kotresh Totagar <kotreshtotagar22@gmail.com>
2018-05-13 23:48:11 +0530

    fixing commit mistake

--- ReactJS-MOBX/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './pages/layout';
-import store from './store/todosStore';
-
-
-ReactDOM.render(<App  store={store}/>, document.getElementById('root'));
